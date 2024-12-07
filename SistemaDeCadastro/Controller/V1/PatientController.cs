342e3c291ab76b2de2568274133c898cff9d63b2
Maria Cecília Araújo <mclemos65@gmail.com>
2024-12-07 11:56:37 -0300

    inicio da controller

---
+++ SistemaDeCadastro/Controller/V1/PatientController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeCadastro.APP.Interface;
+
+namespace SistemaDeCadastro.Controller.V1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PatientController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        private readonly IPatientApp _patientApp;
+
+        public PatientController(IConfiguration configuration, IPatientApp patientApp)
+        {
+            this._configuration = configuration;
+            this._patientApp = patientApp;
+        }
+
+        [HttpGet("GetPatientById")]
+        public async Task<IActionResult> GetPatientById(long id)
+        {
+            var patient = await _patientApp.GetPatientById(id);
+            return Ok(patient);
+        }
+
+    }
+}

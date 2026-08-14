9e57a0306a88ed12819e0593aa72e831db23e886
Maria Cecília Araújo <mclemos65@gmail.com>
2026-08-14 10:40:17 -0300

    .

--- SistemaDeCadastro.Domain/Filters/EmployeeFilterDTO.cs
+++ SistemaDeCadastro.Domain/Filters/EmployeeFilterDTO.cs
@@ -5,12 +5,12 @@ namespace SistemaDeCadastro.Domain.Filters
     public class EmployeeFilterDTO
     {
         public long Id { get; set; }
-        public string Name { get; set; }
-        public string Cpf { get; set; }
-        public string Position { get; set; }
+        public string? Name { get; set; }
+        public string? Cpf { get; set; }
+        public string? Position { get; set; }
         public DateTime? AdmissionDate { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
         public long? DepartmentId { get; set; }
         public int Page { get; set; } = 1;
     }

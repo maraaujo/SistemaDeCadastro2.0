71c751aa4f5300d88303bbabdf3f4ecc4c731d61
maraaujo <mclemos65@gmail.com>
2026-08-18 19:40:19 -0300

    .

--- SistemadeCadastro.Test/TestDbContextFactory.cs
+++ SistemadeCadastro.Test/TestDbContextFactory.cs
@@ -9,10 +9,10 @@ namespace SistemadeCadastro.Test
         {
             var optionsBuilder = new DbContextOptionsBuilder<SistemaDeCadastroContext>();
 
-            optionsBuilder.UseMySql(
-               "server=127.0.0.1;port=3306;database=sistema_cadastro_tcc;user=sistema_user;password=irmaos03;",
-                new MySqlServerVersion(new Version(8, 0, 46))
-            );
+            //optionsBuilder.UseMySql(
+            //   "server=127.0.0.1;port=3306;database=sistema_cadastro_tcc;user=sistema_user;password=irmaos03;",
+            //    new MySqlServerVersion(new Version(8, 0, 46))
+            //);
 
             return new SistemaDeCadastroContext(optionsBuilder.Options, new TestCurrentUserService());
         }
